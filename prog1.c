bd568e408686fe5b247c9731e126e067504a35c0
Ted Thornton <tthorn00@g.uafs.edu>
2022-09-12 18:53:43 +0000

    update

---
+++ prog1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+  int numarray[5] ={ 1,2,3,4,5};	
+
+  int index = 0;
+
+  for(index;index<5;index++){
+    printf("%d\n", numarray[index]);
+
+  }
+  return 0;
+
+
+}
